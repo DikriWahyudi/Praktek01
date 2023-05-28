72da8b7016358a8860cd3ec89f2f47808137ff31
DikriWahyudi <wahyudidikri135@gmail.com>
2023-05-28 08:46:51 +0800

    Mengisi Nilai Atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -4,6 +4,10 @@ public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Dikri = new Mahasiswa();
         
+        Dikri.Nama = "Dikri Wahyudi";
+        Dikri.NPM = "2110010528";
+        Dikri.IPK = 4.0;
         
+        Dikri.CetakInfo();
     }
 }
