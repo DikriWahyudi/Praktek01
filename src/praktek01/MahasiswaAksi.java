52177c53042242d42f074fdf41338a6bc37d91f0
DikriWahyudi <wahyudidikri135@gmail.com>
2023-05-28 08:46:21 +0800

    Instance Object

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -2,6 +2,8 @@ package praktek01;
 
 public class MahasiswaAksi {
     public static void main(String[] args) {
-                
+        Mahasiswa Dikri = new Mahasiswa();
+        
+        
     }
 }
